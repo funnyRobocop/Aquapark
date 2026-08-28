c0eca6631fd236efd010e1586bede729b00b956a
Alikin_Alexey <funnyrobocop@gmail.com>
2026-08-29 00:00:21 +0300

    upd

--- Assets/Project/Scripts/Input/NetworkInputData.cs
+++ Assets/Project/Scripts/Input/NetworkInputData.cs
@@ -1,12 +1,15 @@
 using Fusion;
 using UnityEngine;
 
-public struct NetworkInputData : INetworkInput
+namespace NonameGame
 {
-    public Vector2 Move;
-    public Vector2 Look;
-    public NetworkButtons Buttons;
+    public struct NetworkInputData : INetworkInput
+    {
+        public Vector2 Move;
+        public Vector2 Look;
+        public NetworkButtons Buttons;
 
-    public const int BUTTON_JUMP = 0;
-    public const int BUTTON_PUSH = 1; // на будущее (ЛКМ)
+        public const int BUTTON_JUMP = 0;
+        public const int BUTTON_PUSH = 1; // на будущее (ЛКМ)
+    }
 }
